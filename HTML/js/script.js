86438a779da5efcfd59ab451d7bb1c79b5ce573f
Jens Wittmann <post@jens-wittmann.de>
2015-10-26 20:52:04 +0100

    first animation

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -8,35 +8,51 @@ $(document).ready(function() {
 	// firefly #1
     var beziers = BezierPlugin.bezierThrough([{x:0, y:0}, {x:250, y:400}, {x:500, y:0}]);
     console.log(beziers);
-    var fly = document.getElementById("fly");
-    TweenMax.to(fly, 5, {bezier:{type:"thru", values:[
+    var fly = document.getElementById("firefly");
+    TweenMax.to(fly, 10, {bezier:{type:"thru", values:[
         {x:100, y:250},
         {x:300, y:0},
-        {x:500, y:400}
+        {x:500, y:400},
+        {x:400, y:200},
+        {x:500, y:300},
+        {x:800, y:200},
+        {x:500, y:500}
     ], autoRotate:true}, ease:Power1.easeInOut});
     
     // timing
     $('.title-animation-elm').hide();
     window.setTimeout(function() {
-	 
-		// textillate
 		$('.title-animation-elm').fadeIn().textillate({
 			in: {
 				effect: 'bounceIn'
 			}
 		});
-		
-    }, 7000);
+    }, 5000);
+    window.setTimeout(function() {
+		$('.step-1').animate({
+			marginTop: '-800px'
+		}, 5000, function() {
+			goToStep(2);
+		});
+    }, 8000);
+    window.setTimeout(function() {
+		goToStep(3);
+    }, 20000);
+    window.setTimeout(function() {
+		goToStep(5);
+    }, 23000);
     
     // slider nav
     var sliderNext = function() {
-	    console.log('next slide');
-    }
+	    $('.slick').slick('slickNext'); 
+    };
 	
 	// steps
 	var step = 1;
 	$('.next').on('click', function() {
-		console.log('hey');
+		goToStep(step);
+	});
+	var goToStep = function(step) {
 		if (step == 9) {
 			step = 3;
 		}
@@ -53,7 +69,7 @@ $(document).ready(function() {
 			});
 			run6();
 		}
-	});
+	}
 		
 });
 
